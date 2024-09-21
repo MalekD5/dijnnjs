0e9d53561c59a3b70b9af06bad9fbfe502c6dfa3
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-09-22 00:34:41 +0300

    build: remove lefthook

--- packages/core/src/processor.ts
+++ packages/core/src/processor.ts
@@ -1,1 +1,6 @@
-import type { BaseCredential } from "./credential";import type { BaseUser } from "./user";export type Processor<C extends BaseCredential, U extends BaseUser> = {	process(credential: C): Promise<U>;};
+import type { BaseCredential } from "./credential";
+import type { BaseUser } from "./user";
+
+export type Processor<C extends BaseCredential, U extends BaseUser> = {
+	process(credential: C): Promise<U>;
+};
